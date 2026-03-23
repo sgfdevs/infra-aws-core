11ba4d751a88c5527f7eab0c285ad4a83e949491
Levi Zitting <me@levizitting.com>
2026-03-23 10:39:23 -0500

    fix: align global stack resources with current state

--- src/tf/main.tf
+++ src/tf/main.tf
@@ -1,26 +1,50 @@
-resource "aws_s3_bucket" "tfstate_global" {
-  bucket = "sgfdevs-global-tfstate"
+locals {
+  backends = {
+    global = {
+      bucket_name = "sgfdevs-global-tfstate"
+      table_name  = "sgfdevs-global-tflock"
+      environment = "global"
+    }
+    prod = {
+      bucket_name = "sgfdevs-environment-prod-tfstate"
+      table_name  = "sgfdevs-environment-prod-tflock"
+      environment = "prod"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "tfstate" {
+  for_each = local.backends
+  region   = var.aws_region
+
+  bucket = each.value.bucket_name
 
   tags = merge(
     var.tags,
     {
-      Name        = "sgfdevs-global-tfstate"
-      Environment = "global"
-      ManagedBy   = "OpenTofu"
+      "Name"        = each.value.bucket_name
+      "Environment" = each.value.environment
+      "ManagedBy"   = "OpenTofu"
     }
   )
 }
 
-resource "aws_s3_bucket_versioning" "tfstate_global" {
-  bucket = aws_s3_bucket.tfstate_global.id
+resource "aws_s3_bucket_versioning" "tfstate" {
+  for_each = local.backends
+  region   = var.aws_region
+
+  bucket = aws_s3_bucket.tfstate[each.key].id
 
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate_global" {
-  bucket = aws_s3_bucket.tfstate_global.id
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
+  for_each = local.backends
+  region   = var.aws_region
+
+  bucket = aws_s3_bucket.tfstate[each.key].id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -29,8 +53,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate_global" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "tfstate_global" {
-  bucket = aws_s3_bucket.tfstate_global.id
+resource "aws_s3_bucket_public_access_block" "tfstate" {
+  for_each = local.backends
+  region   = var.aws_region
+
+  bucket = aws_s3_bucket.tfstate[each.key].id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -38,18 +65,24 @@ resource "aws_s3_bucket_public_access_block" "tfstate_global" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_ownership_controls" "tfstate_global" {
-  bucket = aws_s3_bucket.tfstate_global.id
+resource "aws_s3_bucket_ownership_controls" "tfstate" {
+  for_each = local.backends
+  region   = var.aws_region
+
+  bucket = aws_s3_bucket.tfstate[each.key].id
 
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
 }
 
-resource "aws_dynamodb_table" "tflock_global" {
-  name         = "sgfdevs-global-tflock"
+resource "aws_dynamodb_table" "tflock" {
+  for_each = local.backends
+
+  name         = each.value.table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
+  region       = var.aws_region
 
   attribute {
     name = "LockID"
@@ -59,9 +92,9 @@ resource "aws_dynamodb_table" "tflock_global" {
   tags = merge(
     var.tags,
     {
-      Name        = "sgfdevs-global-tflock"
-      Environment = "global"
-      ManagedBy   = "OpenTofu"
+      "Name"        = each.value.table_name
+      "Environment" = each.value.environment
+      "ManagedBy"   = "OpenTofu"
     }
   )
 }
