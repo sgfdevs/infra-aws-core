61a712d63dea8c37a5d167c55e3f84aa0c741c7d
Levi Zitting <me@levizitting.com>
2026-07-22 07:35:23 -0700

    feat: add SES vdm and default configuration set

--- src/tf/ses.tf
+++ src/tf/ses.tf
@@ -5,17 +5,47 @@ locals {
   ])
 }
 
+resource "aws_sesv2_account_vdm_attributes" "current" {
+  vdm_enabled = "ENABLED"
+
+  dashboard_attributes {
+    engagement_metrics = "DISABLED"
+  }
+
+  guardian_attributes {
+    optimized_shared_delivery = "DISABLED"
+  }
+}
+
+resource "aws_sesv2_configuration_set" "transactional" {
+  configuration_set_name = "transactional"
+
+  vdm_options {
+    dashboard_options {
+      engagement_metrics = "DISABLED"
+    }
+
+    guardian_options {
+      optimized_shared_delivery = "DISABLED"
+    }
+  }
+
+  tags = {
+    ManagedBy = "OpenTofu"
+  }
+
+  depends_on = [aws_sesv2_account_vdm_attributes.current]
+}
+
 resource "aws_sesv2_email_identity" "domain" {
   for_each = local.ses_email_identities
 
-  email_identity = each.value
+  email_identity         = each.value
+  configuration_set_name = aws_sesv2_configuration_set.transactional.configuration_set_name
 
-  tags = merge(
-    var.tags,
-    {
-      ManagedBy = "OpenTofu"
-    }
-  )
+  tags = {
+    ManagedBy = "OpenTofu"
+  }
 }
 
 resource "aws_sesv2_email_identity_mail_from_attributes" "domain" {
